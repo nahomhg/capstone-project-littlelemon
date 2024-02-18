6a706ac92bf583697cbda50e666e49e09324fabe
nahomhg <nahom.gh@outlook.com>
2024-02-18 19:01:17 +0000

    update changes to views.py on restaurant app and minor changes on frontend app

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,4 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -7,14 +6,8 @@ from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import action;
-from rest_framework.exceptions import ValidationError
-from django.http import HttpResponse, Http404
-from datetime import datetime
-from django.core import serializers
+from datetime import datetime, date
 from django.shortcuts import get_object_or_404
-import json, logging
-
-logger = logging.getLogger(__name__)
 
 # Create your views here.
 class ThrottleAPIMixin:
@@ -31,71 +24,48 @@ class SingleItemView(RetrieveUpdateDestroyAPIView, ThrottleAPIMixin, Authenticat
     queryset = Menu.objects.all();
     serializer_class = MenuSerializer
     
-class BookingViewSet(ModelViewSet, RetrieveUpdateDestroyAPIView, ThrottleAPIMixin, AuthenticationMixin):
-    queryset = Booking.objects.all();
+
+class BookingCreate(ListCreateAPIView):
+    queryset = Booking.objects.filter(booking_date__contains=date.today());
     serializer_class = BookingSerializer
-
-    def list(self, request, *args, **kwargs):
-        serializer = BookingSerializer(instance=self.queryset,many=True);
-        return Response(serializer.data, status=200);
-
-    @action(detail=True, methods=['GET'])
-    def booking_detail(self, request, *args, **kwargs):
-        reservation_date = kwargs['booking_date']
-        reservation_slot = kwargs['booking_slot']
-        if Validate_Date.is_date(reservation_date) and validate_time_slot(reservation_slot):
-            try:
-                booking = Booking.objects.filter(booking_date=reservation_date, booking_slot=reservation_slot);
-                serialize_booking = BookingSerializer(instance=booking);
-                return Response(data=serialize_booking.data, status=200)
-            except ValidationError:
-                logger.debug("Validation Error "+str(ValidationError))
-            except ValueError:
-                logger.debug("ERROR: Value Error "+str(ValueError));
-        
-        return Response({'retrieve':'data'}, status=200)
-
-
-    def retrieve(self, request, *args, **kwargs):
-        reservation_date = kwargs['booking_date']
-        print('data kwargs = '+str(kwargs));
-        if Validate_Date.is_date(reservation_date):
-            try:
-                bookings = Booking.objects.filter(booking_date=reservation_date);
-                print('data '+str(bookings))
-                serialize_booking = BookingSerializer(bookings, many=True);
-                return Response(data=serialize_booking.data, status=200)
-            except ValidationError:
-                print("Validation Error "+str(ValidationError))
-            except ValueError:
-                print("ERROR: Value Error "+str(ValueError));
-            except Http404:
-                print("404 Error: "+str(Http404))
-        
-        return Response({'retrieve':'data'}, status=200)
 
     def create(self, request, *args, **kwargs):
         form_data_json = JSONParser().parse(request);
         form_serializer = BookingSerializer(data=form_data_json);
         if form_serializer.is_valid():
             form_serializer.save();      
-            logger.info("data is = "+str(form_serializer)) 
-            print("data saved")
             return Response(form_serializer.data, status=200)
         else:
-            print("invalid form"+str(form_serializer.error_messages)+"\nERROR: "+str())
             return Response({'ERROR':f'{form_serializer.errors}'},status=403);
-    
-def validate_time_slot(time_slot):
-    return True if 10 < time_slot <= 20 else False;
 
+
+class BookingDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all();
+    serializer_class = BookingSerializer;
     
+    def get_object(self):
+        from django.core.serializers import serialize
+        reservation_date = self.kwargs['booking_date']
+        reservation_slot = self.kwargs['booking_slot']
+        self.queryset = get_object_or_404(Booking, booking_date=reservation_date, booking_slot=reservation_slot);
+        return self.queryset;
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs);
 
 class Validate_Date(datetime):
-    def is_date(value):
-        try:
-            date_value = datetime.strptime(value, '%Y-%m-%d').date()
+    
+    @staticmethod
+    def is_valid_date(date_str):
+        curr_date = datetime.today().date()
+        if date_str >= str(curr_date):
             return True;
-        except ValueError:
+        else:
             return False;
-
+    
+    def validate_time_slot(time_slot):
+        if 10 < int(time_slot) <= 20:
+            return True
+        else:
+            return False;
+       
